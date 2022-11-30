83d4e4b33a9072df2fbf6be578daa40bca2e3287
Brucecrix <119473092+Brucecrix@users.noreply.github.com>
2022-11-29 22:38:21 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+name = "My name is"
+firstName = "Christian"
+lastName = "Wiah"; age = "100!"
+print("My name is Christian Bruce Wiah\n"" - I am 100")
+
+john = 10
+mary = 15
+sarah = "This is nice."
+print(sarah)
+
+a = 011.2 < 53
+b = 12 > 12
+print(bool(a))
+print(bool(b))
+
+userName = input("Please enter your full name:")
+userAge = input("Please enter your age:")
+print(userName, "-", userAge, "years old")
+
